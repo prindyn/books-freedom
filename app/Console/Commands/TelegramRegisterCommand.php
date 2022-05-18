0579fc1ecab29906577eb80959f6ffbbe541a52c
vasyl.prindyn <vasyl.prindyn@vakoms.com>
2022-05-18 06:31:30 +0300

    ngrok auto start

--- app/Console/Commands/TelegramRegisterCommand.php
+++ app/Console/Commands/TelegramRegisterCommand.php
@@ -28,17 +28,20 @@ class TelegramRegisterCommand extends Command
     public function handle()
     {
         $url = 'https://api.telegram.org/bot'
-                .config('botman.telegram.token')
-                .'/setWebhook';
+            . config('botman.telegram.token')
+            . '/setWebhook';
 
         $remove = $this->option('remove', null);
 
-        if (! $remove) {
-            $url .= '?url=' . (env('TG_WEBHOOK_URL') ?? $this->ask('What is the target url for the telegram bot?'));
+        if (!$remove) {
+            $webhookUrl = $this->ngrok();
+            $url .= '?url=' . ($webhookUrl ?? $this->ask('What is the target url for the telegram bot?'));
             $url .= '/botman';
+        } else {
+            shell_exec("killall ngrok");
         }
 
-        $this->info('Using '.$url);
+        $this->info('Using ' . $url);
 
         $this->info('Pinging Telegram...');
 
@@ -47,13 +50,62 @@ class TelegramRegisterCommand extends Command
         if ($output->ok == true && $output->result == true) {
             $this->info(
                 $remove
-                ? 'Your bot Telegram\'s webhook has been removed!'
-                : 'Your bot is now set up with Telegram\'s webhook!'
+                    ? 'Your bot Telegram\'s webhook has been removed!'
+                    : 'Your bot is now set up with Telegram\'s webhook!'
             );
         }
 
         if ($this->option('output')) {
             dump($output);
         }
+    }
+
+    protected function ngrok($port = '8000')
+    {
+        shell_exec("ngrok http $port > /dev/null &");
+        sleep(1);
+        $ngrok = json_decode(file_get_contents('http://localhost:4040/api/tunnels'), true);
+        $env = getenv();
+
+        foreach ($ngrok['tunnels'] as $tunnel) {
+
+            if (strpos($tunnel['public_url'], 'https://') !== false) {
+                $env['TG_WEBHOOK_URL'] =  $tunnel['public_url'];
+                $this->updateEnv($env);
+            }
+        }
+        return $env['TG_WEBHOOK_URL'];
+    }
+
+    protected function updateEnv($data = array())
+    {
+        if (!count($data)) {
+            return;
+        }
+
+        $pattern = '/([^\=]*)\=[^\n]*/';
+
+        $envFile = base_path() . '/.env';
+        $lines = file($envFile);
+        $newLines = [];
+        foreach ($lines as $line) {
+            preg_match($pattern, $line, $matches);
+
+            if (!count($matches)) {
+                $newLines[] = $line;
+                continue;
+            }
+
+            if (!key_exists(trim($matches[1]), $data)) {
+                $newLines[] = $line;
+                continue;
+            }
+
+            $line = trim($matches[1]) . "={$data[trim($matches[1])]}\n";
+            $newLines[] = $line;
+        }
+
+        $newContent = implode('', $newLines);
+        file_put_contents($envFile, $newContent);
     }
 }
